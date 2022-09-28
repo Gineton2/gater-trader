c92c6e2d4b39885cf33f3ab67d2e7a93420fdf06
RINO-GAELICO <ducciorocca@gmail.com>
2022-09-28 09:52:02 -0700

    favicon working

--- application/app.js
+++ application/app.js
@@ -32,9 +32,10 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use("/", indexRouter); // route middleware from ./routes/index.js
 app.use("/users", usersRouter); // route middleware from ./routes/users.js
-app.use(favicon(__dirname + '/public/favicon.ico'));
+
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); 
 
 app.listen(1234, () => console.log('Server running on port 1234'));
 
