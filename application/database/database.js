ecd759aa5939d3f78d0552b0fe926495b613e2ae
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-12-03 11:42:42 -0800

    changed setting in  database.js to EC2

--- application/database/database.js
+++ application/database/database.js
@@ -19,10 +19,10 @@ const mysql = require('mysql2');
 const db = mysql.createPool({
     
     // EC2
-    // host: "localhost",
-    // database:"db_test",
-    // user: "root", 
-    // password: "TEAM07team07",
+    host: "localhost",
+    database:"db_test",
+    user: "root", 
+    password: "TEAM07team07",
 
     // local Duccio
     // host: 'localhost',
@@ -31,10 +31,10 @@ const db = mysql.createPool({
     // password : 'ABB10iote'
 
     // // local Yoshi
-    host: 'localhost',
-    user : 'root',
-    database : 'copy_EC2_DB',
-    password : 'passyoshi'
+    // host: 'localhost',
+    // user : 'root',
+    // database : 'copy_EC2_DB',
+    // password : 'passyoshi'
 
     //  // // local Rai'd M.
     // host: 'localhost',
