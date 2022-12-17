cf9da6776a43044fceb9ba8492ff9b9ac3ea4a2c
Gineton Alencar II <Gineton2@users.noreply.github.com>
2022-12-17 11:49:59 -0800

    Fix database settings

--- application/database/database.js
+++ application/database/database.js
@@ -19,10 +19,10 @@ const mysql = require('mysql2');
 const db = mysql.createPool({
     
     // EC2
-    // host: "localhost",
-    // database:"db_test",
-    // user: "root", 
-    // password: "TEAM07team07",
+    host: "localhost",
+    database:"db_test",
+    user: "root", 
+    password: "TEAM07team07",
 
     // local Duccio
     // host: 'localhost',
@@ -36,11 +36,11 @@ const db = mysql.createPool({
     // database : 'copy_EC2_DB',
     // password : 'passyoshi'
 
-     // // local Rai'd M.
-    host: 'localhost',
-    user : 'root',
-    database : 'teamschema',
-    password : 'nap765gUp!'
+    // local Rai'd M.
+    // host: 'localhost',
+    // user : 'root',
+    // database : 'teamschema',
+    // password : 'nap765gUp!'
 });
 
 module.exports = db.promise();
